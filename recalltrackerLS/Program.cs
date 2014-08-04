eb9be63ed02e1166d0a2143232b0134f1cba1bd0
irvine <a@a>
2014-08-04 05:40:53 +0200

    comment

---
+++ recalltrackerLS/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+
+namespace recalltrackerLS {
+    class Program {
+        static void Main(string[] args) {
+            Game.OnGameProcessPacket += Game_OnGameProcessPacket;
+            Drawing.OnDraw += Drawing_OnDraw;
+            RecallList = new List<Recall>();
+        }
+        
+        static void Drawing_OnDraw(EventArgs args) {
+            float x = Drawing.Width - 605;
+            float y = Drawing.Height - 150;
+            
+            foreach (Recall r in RecallList) {
+                if (r.update()) {
+
+                } else {
+                    if (r.LastAction == Recall.RecallState.Recalling) {
+                        Drawing.DrawText(x, y, System.Drawing.Color.Beige, r.ToString());
+                    } else if (r.LastAction == Recall.RecallState.Ported) {
+                        Drawing.DrawText(x, y, System.Drawing.Color.GreenYellow, r.ToString());
+                    } else if (r.LastAction == Recall.RecallState.Cancelled) {
+                        Drawing.DrawText(x, y, System.Drawing.Color.Red, r.ToString());
+                    }
+                    y += 15;
+                }
+            }
+        }
+        static void Recall_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e) {
+            
+        }
+
+        static List<Recall> RecallList;
+
+        static void Game_OnGameProcessPacket(GamePacketEventArgs args) {
+            if (args.PacketData[0] == 0xD8 || args.PacketData[0] == 0xD7) {
+                var stream = new System.IO.MemoryStream(args.PacketData);
+                var byteRead = new System.IO.BinaryReader(stream);
+
+                byteRead.ReadByte();
+                byteRead.ReadBytes(4);
+                var netidbytes = byteRead.ReadBytes(4);
+                var networkId = System.BitConverter.ToInt32(netidbytes, 0);
+
+                byteRead.ReadBytes(0x42);
+                string s = System.BitConverter.ToString(byteRead.ReadBytes(6));
+                var state = Recall.RecallState.Recalling;
+                if (string.Equals("00-00-00-00-00-00", s)) {
+                    state = Recall.RecallState.Unknown;
+                }
+                byteRead.Close();
+
+                var unit = ObjectManager.GetUnitByNetworkId<GameObject>(networkId);
+                if (unit == null || !unit.IsValid) {
+                    return;
+                }
+                if (unit.Team == ObjectManager.Player.Team) {
+                    return;
+                }
+                
+                HandleRecall((Obj_AI_Hero)unit, state);
+            }
+        }
+        public static void HandleRecall(Obj_AI_Hero hero, Recall.RecallState recallType) {
+            Recall recall = null;
+            recall = RecallList.Where(x => x.Player.NetworkId == hero.NetworkId).FirstOrDefault();
+            if (recall == null) {
+                recall = new Recall(hero);
+                recall.LastAction = recallType;
+                RecallList.Add(recall);
+            } else {
+                recall.LastAction = recallType;
+            }
+        }
+    }
+}
---
+++ recalltrackerLS/Recall.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+
+namespace recalltrackerLS {
+    public class Recall {
+        public enum RecallState {
+            Recalling,
+            Ported,
+            Cancelled,
+            Unknown
+        }
+
+        private int _lastActionTick;
+
+        private RecallState _lastAction;
+        public RecallState LastAction { 
+            get { return _lastAction; } 
+            set { 
+                _lastAction = value;
+
+                if (_lastAction == RecallState.Unknown) {
+                    if (TimeElapsedms > (recallTime-40)) {
+                        LastAction = RecallState.Ported;
+                    } else {
+                        LastAction = RecallState.Cancelled;
+                    }
+                } else {
+                    _lastActionTick = Environment.TickCount;
+                }
+            } 
+        }
+        private Obj_AI_Hero _player;
+        public Obj_AI_Hero Player { get { return _player; } }
+        public int TimeElapsedms { get { return (Environment.TickCount - _lastActionTick); } }
+
+        public Recall(Obj_AI_Hero target) {
+            _player = target;
+            recallTime = (_player.Masteries.Where(x => x.Page == MasteryPage.Utility && x.Id == 0x41 && x.Points == 1).Count() == 0) ? 8000 : 7000;
+        }
+        private int recallTime;
+        public bool update() { 
+            if (_lastAction == RecallState.Ported) {
+                return (TimeElapsedms>=25000)? true:false;
+            } else if (_lastAction == RecallState.Cancelled) {
+                return (TimeElapsedms >= 3000) ? true : false;
+            } else if (_lastAction == RecallState.Recalling) {
+                return (TimeElapsedms >= 8000) ? true : false;
+            } else {
+                return true;
+            }
+        }
+
+        public override string ToString() {
+            if (LastAction == RecallState.Recalling) {
+                return String.Format("{0} recalling ({1:0.00})", Player.BaseSkinName, (((float)(TimeElapsedms - recallTime)) / (float)1000));
+            } else if (LastAction == RecallState.Ported) {
+                return String.Format("{0} Ported ({1:0.00})", Player.BaseSkinName, ((float)TimeElapsedms / (float)1000));
+            } else if (LastAction == RecallState.Cancelled) {
+                return String.Format("{0} Cancelled",Player.BaseSkinName);
+            } else {
+                return "";
+            }
+        }
+    }
+
+    
+}
